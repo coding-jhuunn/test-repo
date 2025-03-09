d4740b08f16a15fb0b4644d05c41e16144d88f1e
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-09 12:48:58 +0800

    setup taiwlind

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>Header</div>
+  <div>
+    <div class="text-3xl font-bold underline">Header</div>
+  </div>
 </template>
 
 <script setup>
 console.log("HEader");
 </script>
+
+<style scoped></style>
