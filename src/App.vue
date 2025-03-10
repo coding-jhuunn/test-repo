456abbbc84b58875de86a3d65b6d5d2ed12132c2
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-10 18:04:00 +0800

    add componets

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,14 @@
   <div>
     <MyHeader></MyHeader>
     <MyForm @updateTodoList="updateTodoList"></MyForm>
-    <div>
-      {{ todolist }}
-    </div>
+    <MyTodoList :myTodoList="todolist"></MyTodoList>
   </div>
 </template>
 <script setup>
 import { reactive, ref } from "vue";
 import MyHeader from "./components/MyHeader.vue";
 import MyForm from "./components/MyForm.vue";
+import MyTodoList from "./components/MyTodoList.vue";
 const todolist = ref([]);
 
 const updateTodoList = (item) => {
