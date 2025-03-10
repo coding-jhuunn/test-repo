216e7cb8ea093717707d870da497b4649009a6dd
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-10 17:46:11 +0800

    accepts props from child MyForm

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
     <MyHeader></MyHeader>
+    <MyForm @updateTodoList="updateTodoList"></MyForm>
+    <div>
+      {{ todolist }}
+    </div>
   </div>
 </template>
 <script setup>
+import { reactive, ref } from "vue";
 import MyHeader from "./components/MyHeader.vue";
+import MyForm from "./components/MyForm.vue";
+const todolist = ref([]);
+
+const updateTodoList = (item) => {
+  todolist.value.push(item);
+};
 </script>
