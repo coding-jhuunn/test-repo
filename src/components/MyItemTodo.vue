ad3d42c089d385a42f687772d419f23ab2adad62
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-10 18:03:51 +0800

    pass props and loop

---
+++ src/components/MyItemTodo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>{{ task.title }}</p>
+    <p v-show="task.priorityTask">priority</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(["task"]);
+</script>
---
+++ src/components/MyTodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>List of To do</div>
+    <MyItemTodo
+      v-for="item in myTodoList"
+      :key="item.id"
+      :task="item"
+    ></MyItemTodo>
+  </div>
+</template>
+<script setup>
+import MyItemTodo from "./MyItemTodo.vue";
+
+const props = defineProps(["myTodoList"]);
+</script>
