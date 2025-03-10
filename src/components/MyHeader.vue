a61c88232ecbfac9747c8c9bbe78fe24323bcc77
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-10 12:59:37 +0800

    setup header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <div class="font-display bg-amber-950">Header</div>
-    <button class=".btn">red</button>
+  <div
+    class="flex flex-row justify-evenly items-center content-center border-b-1 h-12"
+  >
+    <div class="">Todo LIST</div>
+    <div class="">
+      <a href="https://github.com/coding-jhuunn">coding-jhuunn</a>
+    </div>
   </div>
 </template>
 
