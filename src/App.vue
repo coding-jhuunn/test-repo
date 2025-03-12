58c9fd6c670fc4b3c099742c22eb45b280fd8ca2
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-12 15:24:32 +0800

    add Sorted feature

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,15 @@
     <MyHeader></MyHeader>
     <div class="flex flex-col justify-center items-center">
       <MyForm class="m-5 border-1" @updateTodoList="updateTodoList"></MyForm>
+      <div>
+        <button @click="IsSorted = !IsSorted">
+          {{ IsSorted ? "Sort" : "Sorted" }}
+        </button>
+      </div>
+
       <MyTodoList
         class="m-5 border-1"
-        :myTodoList="todolist"
+        :myTodoList="IsSorted ? todolist : sortTask"
         @deleteItemTodo="deleteItemTodo"
         @toggleItemTodo="toggleItemTodo"
       ></MyTodoList>
@@ -13,7 +19,7 @@
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import MyHeader from "./components/MyHeader.vue";
 import MyForm from "./components/MyForm.vue";
 import MyTodoList from "./components/MyTodoList.vue";
@@ -39,6 +45,15 @@ const todolist = ref([
     priorityTask: false,
   },
 ]);
+const IsSorted = ref(true);
+
+const sortTask = computed(() => {
+  return [...todolist.value].sort((a, b) => {
+    return IsSorted.value
+      ? a.priorityTask - b.priorityTask
+      : b.priorityTask - a.priorityTask;
+  });
+});
 
 const updateTodoList = (item) => {
   todolist.value.push(item);
