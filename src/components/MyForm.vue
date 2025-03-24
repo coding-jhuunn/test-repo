8aceaadb605c67e33a2c91b8defb7e87b4ad468d
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-24 15:09:58 +0800

    add stlyes to Form

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -1,47 +1,56 @@
 <template>
-  <div class="p-6 bg-white shadow-lg rounded-lg">
-    <form
-      @submit.prevent="submitForm"
-      class="flex flex-row justify-evenly items-center"
-    >
-      <div class="bg-red-50 flex flex-col h-12">
-        <label class="block font-medium">Title</label>
+  <div class="flex justify-center mb-5 p-5">
+    <!-- text-xs sm:text-sm md:text-base lg:text-lg -->
+    <form class="p-5 w-[300px] shadow-xl" @submit.prevent="submitForm">
+      <div class="mb-2">
         <input
+          placeholder="Input Here ..."
           v-model="titleTask"
           type="text"
-          class="border-b-2"
-          placeholder="Input Here"
+          class="block w-full bg-gray-50 p-1 pl-2 focus:outline-none focus:ring-1 focus:ring-blue-500 rounded-lg"
         />
+        <p class="ml-1 text-red-500 text-[10px]" v-if="errorShow">
+          {{ errorMessage }}
+        </p>
       </div>
-      <div
-        class="bg-green-50 flex justify-evenly align-center items-center h-12"
-      >
-        <input v-model="priorityTask" type="checkbox" />
-        <label class="font-medium">Priority</label>
-      </div>
-      <div class="">
-        <button
-          class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-        >
-          Submit
-        </button>
+      <div>
+        <div class="flex mb-2 pl-1">
+          <input v-model="priorityTask" type="checkbox" class="mr-1" />
+          <label class="text-sm md:text-base">Priority</label>
+        </div>
+        <div class="flex justify-center items-center w-100%">
+          <button
+            class="w-[150px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </div>
       </div>
     </form>
   </div>
 </template>
 <script setup>
-import { ref, reactive, defineEmits } from "vue";
+import { ref, defineEmits } from "vue";
 
 const emit = defineEmits(["updateTodoList"]);
-
-const submitForm = () => {
-  emit("updateTodoList", {
-    id: Date.now(),
-    title: titleTask.value,
-    priorityTask: priorityTask.value,
-  });
-};
-
 const titleTask = ref("");
 const priorityTask = ref(false);
+const errorShow = ref(false);
+const errorMessage = ref("");
+
+const submitForm = () => {
+  if (titleTask.value.length === 0) {
+    errorMessage.value = "Please enter a value";
+    errorShow.value = true;
+  } else {
+    errorShow.value = false;
+    emit("updateTodoList", {
+      id: Date.now(),
+      title: titleTask.value,
+      priorityTask: priorityTask.value,
+    });
+    titleTask.value = "";
+    priorityTask.value = false;
+  }
+};
 </script>
