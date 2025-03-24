172eac096a98c56c1adc54628976e85f520df1c6
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-24 17:32:14 +0800

    add Styles to List n Item

--- src/components/MyItemTodo.vue
+++ src/components/MyItemTodo.vue
@@ -1,29 +1,25 @@
 <template>
-  <div class="max-wd flex justify-between">
-    <div class="flex">
-      <div>
-        {{ task.priorityTask }}
-        <button @click="toggleItem(task.id)">Toggle</button>
+  <div class="flex justify-between items-center mb-1 p-1">
+    <div class="flex p-1">
+      <div class="flex ju items-center mr-1">
+        <button
+          @click="toggleItem(task.id)"
+          class="hover:scale-150 cursor-pointer flex justify-items-center"
+        >
+          <i v-show="task.priorityTask" class="bx bxs-star"></i>
+          <i v-show="!task.priorityTask" class="bx bxs-star-half"></i>
+        </button>
       </div>
-      <div>
+      <div class="flex justify-center items-center">
         {{ task.title }}
       </div>
     </div>
     <div>
-      <button @click="deleteItem(task.id)">Delete</button>
+      <button @click="deleteItem(task.id)">
+        <i class="bx bxs-minus-circle hover:scale-150 cursor-pointer"></i>
+      </button>
     </div>
   </div>
-  <!-- <div
-    :class="{
-      'border-r-20 ': task.priorityTask,
-      'bg-gray-300 text-black': !task.priorityTask,
-    }"
-  >
-    <div>
-      <p>{{ task.title }}</p>
-      <button>Delete</button>
-    </div>
-  </div> -->
 </template>
 
 <script setup>
--- src/components/MyTodoList.vue
+++ src/components/MyTodoList.vue
@@ -1,20 +1,9 @@
-<template>
-  <div class="">
-    <div>List of To do</div>
-    <MyItemTodo
-      v-for="item in myTodoList"
-      :key="item.id"
-      :task="item"
-      @deleteItemTodo="deleteItemTodo"
-      @toggleItemTodo="toggleItemTodo"
-    ></MyItemTodo>
-  </div>
-</template>
 <script setup>
 import { defineEmits } from "vue";
 import MyItemTodo from "./MyItemTodo.vue";
 
 const emit = defineEmits(["deleteItemTodo", "toggleItemTodo"]);
+
 const deleteItemTodo = (index) => {
   emit("deleteItemTodo", index);
 };
@@ -22,5 +11,34 @@ const toggleItemTodo = (index) => {
   emit("toggleItemTodo", index);
 };
 
-const props = defineProps(["myTodoList"]);
+const props = defineProps({
+  myTodoList: Array,
+  toggleTodoList: Function,
+  IsSorted: Boolean,
+});
 </script>
+<template>
+  <div class="flex justify-center items-center">
+    <div class="w-[300px] sm:w-[400px] md:w-[600px]">
+      <div class="flex justify-between text-sm md:text-base mb-2 border-b-2">
+        <div c>LIST TO DO</div>
+        <div>
+          <button
+            @click="toggleTodoList"
+            class="hover:scale-150 cursor-pointer"
+          >
+            <i v-show="IsSorted" class="bx bx-sort"></i>
+            <i v-show="!IsSorted" class="bx bx-shuffle"></i>
+          </button>
+        </div>
+      </div>
+      <MyItemTodo
+        v-for="item in myTodoList"
+        :key="item.id"
+        :task="item"
+        @deleteItemTodo="deleteItemTodo"
+        @toggleItemTodo="toggleItemTodo"
+      ></MyItemTodo>
+    </div>
+  </div>
+</template>
