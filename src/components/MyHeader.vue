fc9cddeca16c30499b7909e73068480a42eb85c1
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-24 12:54:44 +0800

    add styles to Header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-row justify-evenly items-center content-center border-b-1 h-12"
+    class="flex flex-row justify-evenly items-center content-center border-b-1 h-12 text-xs sm:text-sm md:text-base lg:text-lg"
   >
     <div class="">Todo LIST</div>
     <div class="">
