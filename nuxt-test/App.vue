6598f56bfaea89100bf3a070143e9143784702a7
coding-jhuunn <codingatjhunn@gmail.com>
2025-03-31 16:41:06 +0800

    add error Pages

---
+++ nuxt-test/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+<script setup>
+definePageMeta({
+  layout: "default",
+});
+</script>
--- nuxt-test/pages/products/[id].vue
+++ nuxt-test/pages/products/[id].vue
@@ -9,8 +9,15 @@ const { id } = useRoute().params;
 const uri = "https://fakestoreapi.com/products/" + id;
 
 // fetch the product
-
 const { data: product } = await useFetch(uri, { key: id });
+
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Product Not Found",
+    fatal: true,
+  });
+}
 
 definePageMeta({
   layout: "products",
